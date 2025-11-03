efb5e28b011c6095c2dbf22a1a4a3f56414acf2a
Diksha <diksha@macbookpro>
2025-11-02 19:40:08 -0800

    editing display func

--- race.cpp
+++ race.cpp
@@ -14,12 +14,15 @@ using namespace std::chrono;
 
 //display() neatly outputs the results of the races
 //arguments: (4) int[] containing the result data
-void display(int [], int [], int [], int []);
+void display(int [][]);
+
+void display(int [][][]);
 
 int main() {
 	int results[15][4][3];
 
 	for (int i = 0; i < 15; i++){
+		//inserting
 		auto start = high_resolution_clock::now();
 		ifstream fin("codes.txt");
 		string line;
@@ -63,8 +66,7 @@ int main() {
 
 		results[i][1][2] = -1;
 
-		int insert[3];
-
+		//inserting
 		start = high_resolution_clock::now();
 		vect.insert(vect.begin() + (vect.size() / 2), "TESTCODE");
 		end = high_resolution_clock::now();
@@ -85,27 +87,26 @@ int main() {
 		duration = duration_cast<milliseconds>(end - start);
 		results[i][2][2] = duration.count();
 
-		int deleting[3];
-
+		//deleting
 		start = high_resolution_clock::now();
 		vect.erase(vect.begin() + vect.size() / 2);;
 		end = high_resolution_clock::now();
 		duration = duration_cast<milliseconds>(end - start);
-		deleting[0] = duration.count();
+		results[i][3][0] = duration.count();
 
 		start = high_resolution_clock::now();
 		it = lst.begin();
 		advance(it, lst.size() / 2);
 		end = high_resolution_clock::now();
 		duration = duration_cast<milliseconds>(end - start);
-		deleting[1] = duration.count();
+		results[i][3][1] = duration.count();
 
 		start = high_resolution_clock::now();
 		auto itS = st.begin();
 		advance(itS, st.size() / 2);
 		end = high_resolution_clock::now();
 		duration = duration_cast<milliseconds>(end - start);
-		deleting[2] = duration.count();
+		results[i][3][2] = duration.count();
 		
 		if (i != 0){
 			results[i][0][0] += results[i-1][0][0];
@@ -125,32 +126,37 @@ int main() {
                         results[i][3][2] += results[i-1][3][2];
 		}
 	}
-	display(reading, sorting, insert, deleting);
+	display(results);
 
 	return 0;
 }
 
-void display(int r[], int s[], int i[], int d[]){
+void display(int r[15][4][3]){
+	for (int i = 0; i < 15; i++)
+		display(r[i]);
+}
+
+void display(int r[4][3]){
 	cout << right << setw(15) << "Operation"
 		<< right << setw(10) << "Vector"
 		<< setw(10) << "List"
 		<< setw(10) << "Set" << endl;
 	cout << right << setw(15) << "Read"
-		<< right << setw(10) << r[0]
-		<< setw(10) << r[1]
-		<< setw(10) << r[2] << endl;
+		<< right << setw(10) << r[0][0]
+		<< setw(10) << r[0][1]
+		<< setw(10) << r[0][2] << endl;
 	cout << right << setw(15) << "Sort"
-		<< right << setw(10) << s[0]
-		<< setw(10) << s[1]
-		<< setw(10) << s[2] << endl;
+		<< right << setw(10) << r[1][0]
+		<< setw(10) << r[1][1]
+		<< setw(10) << r[1][2] << endl;
 	cout << right << setw(15) << "Insert"
-		<< right << setw(10) << i[0]
-		<< setw(10) << i[1]
-		<< setw(10) << i[2] << endl;
+		<< right << setw(10) << r[2][0]
+		<< setw(10) << r[2][1]
+		<< setw(10) << r[2][2] << endl;
 	cout << right << setw(15) << "Delete"
-		<< right << setw(10) << d[0]
-		<< setw(10) << d[1]
-		<< setw(10) << d[2] << endl;
+		<< right << setw(10) << r[3][0]
+		<< setw(10) << r[3][1]
+		<< setw(10) << r[3][2] << endl;
 }
 /* syntax examples:
    auto start = high_resolution_clock::now()
