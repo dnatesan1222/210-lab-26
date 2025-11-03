870404b8fde800f8032836d0890bf522a1adf2ef
Diksha <diksha@macbookpro>
2025-11-02 19:30:49 -0800

    making a loop for it to run 15x

--- race.cpp
+++ race.cpp
@@ -17,125 +17,144 @@ using namespace std::chrono;
 void display(int [], int [], int [], int []);
 
 int main() {
-    int reading[3];
-    auto start = high_resolution_clock::now();
-    ifstream fin("codes.txt");
-    string line;
-    vector<string> vect;
-    while(getline(fin, line))
-	vect.push_back(line);
-    auto end = high_resolution_clock::now();
-    auto duration = duration_cast<milliseconds>(end - start);
-    reading[0] = duration.count();
- 
-    start = high_resolution_clock::now();
-    ifstream fin1("codes.txt");
-    list<string> lst;
-    while(getline(fin1, line))
-        lst.push_back(line);
-    end = high_resolution_clock::now();
-    duration = duration_cast<milliseconds>(end - start);
-    reading[1] = duration.count();
+	int results[15][4][3];
 
-    start = high_resolution_clock::now();
-    ifstream fin2("codes.txt");
-    set<string> st;
-    while(getline(fin2, line))
-        st.insert(line);
-    end = high_resolution_clock::now();
-    duration = duration_cast<milliseconds>(end - start);
-    reading[2] = duration.count();
+	for (int i = 0; i < 15; i++){
+		auto start = high_resolution_clock::now();
+		ifstream fin("codes.txt");
+		string line;
+		vector<string> vect;
+		while(getline(fin, line))
+			vect.push_back(line);
+		auto end = high_resolution_clock::now();
+		auto duration = duration_cast<milliseconds>(end - start);
+		results[i][0][0] = duration.count();
 
-    int sorting[3];
+		start = high_resolution_clock::now();
+		ifstream fin1("codes.txt");
+		list<string> lst;
+		while(getline(fin1, line))
+			lst.push_back(line);
+		end = high_resolution_clock::now();
+		duration = duration_cast<milliseconds>(end - start);
+		results[i][0][1] = duration.count();
 
-    start = high_resolution_clock::now();
-    sort(vect.begin(), vect.end());
-    end = high_resolution_clock::now();
-    duration = duration_cast<milliseconds>(end - start);
-    sorting[0] = duration.count();
-    
-    start = high_resolution_clock::now();
-    lst.sort();
-    end = high_resolution_clock::now();
-    duration = duration_cast<milliseconds>(end - start);
-    sorting[1] = duration.count();
-    
-    sorting[2] = -1;
+		start = high_resolution_clock::now();
+		ifstream fin2("codes.txt");
+		set<string> st;
+		while(getline(fin2, line))
+			st.insert(line);
+		end = high_resolution_clock::now();
+		duration = duration_cast<milliseconds>(end - start);
+		results[i][0][2] = duration.count();
 
-    int insert[3];
-   
-    start = high_resolution_clock::now();
-    vect.insert(vect.begin() + (vect.size() / 2), "TESTCODE");
-    end = high_resolution_clock::now();
-    duration = duration_cast<milliseconds>(end - start);
-    insert[0] = duration.count();
+		//sorting
+		start = high_resolution_clock::now();
+		sort(vect.begin(), vect.end());
+		end = high_resolution_clock::now();
+		duration = duration_cast<milliseconds>(end - start);
+		results[i][1][0] = duration.count();
 
-    start = high_resolution_clock::now();
-    auto it = lst.begin();
-    advance(it, lst.size() / 2);
-    lst.insert(it, "TESTCODE");
-    end = high_resolution_clock::now();
-    duration = duration_cast<milliseconds>(end - start);
-    insert[1] = duration.count();
+		start = high_resolution_clock::now();
+		lst.sort();
+		end = high_resolution_clock::now();
+		duration = duration_cast<milliseconds>(end - start);
+		results[i][1][1] = duration.count();
 
-    start = high_resolution_clock::now();
-    st.insert("TESTCODE");
-    end = high_resolution_clock::now();
-    duration = duration_cast<milliseconds>(end - start);
-    insert[2] = duration.count();
+		results[i][1][2] = -1;
 
-    int deleting[3];
-    
-    start = high_resolution_clock::now();
-    vect.erase(vect.begin() + vect.size() / 2);;
-    end = high_resolution_clock::now();
-    duration = duration_cast<milliseconds>(end - start);
-    deleting[0] = duration.count();
+		int insert[3];
 
-    start = high_resolution_clock::now();
-    it = lst.begin();
-    advance(it, lst.size() / 2);
-    end = high_resolution_clock::now();
-    duration = duration_cast<milliseconds>(end - start);
-    deleting[1] = duration.count();
+		start = high_resolution_clock::now();
+		vect.insert(vect.begin() + (vect.size() / 2), "TESTCODE");
+		end = high_resolution_clock::now();
+		duration = duration_cast<milliseconds>(end - start);
+		results[i][2][0] = duration.count();
 
-    start = high_resolution_clock::now();
-    auto itS = st.begin();
-    advance(itS, st.size() / 2);
-    end = high_resolution_clock::now();
-    duration = duration_cast<milliseconds>(end - start);
-    deleting[2] = duration.count();
+		start = high_resolution_clock::now();
+		auto it = lst.begin();
+		advance(it, lst.size() / 2);
+		lst.insert(it, "TESTCODE");
+		end = high_resolution_clock::now();
+		duration = duration_cast<milliseconds>(end - start);
+		results[i][2][1] = duration.count();
 
-    display(reading, sorting, insert, deleting);
+		start = high_resolution_clock::now();
+		st.insert("TESTCODE");
+		end = high_resolution_clock::now();
+		duration = duration_cast<milliseconds>(end - start);
+		results[i][2][2] = duration.count();
 
-    return 0;
+		int deleting[3];
+
+		start = high_resolution_clock::now();
+		vect.erase(vect.begin() + vect.size() / 2);;
+		end = high_resolution_clock::now();
+		duration = duration_cast<milliseconds>(end - start);
+		deleting[0] = duration.count();
+
+		start = high_resolution_clock::now();
+		it = lst.begin();
+		advance(it, lst.size() / 2);
+		end = high_resolution_clock::now();
+		duration = duration_cast<milliseconds>(end - start);
+		deleting[1] = duration.count();
+
+		start = high_resolution_clock::now();
+		auto itS = st.begin();
+		advance(itS, st.size() / 2);
+		end = high_resolution_clock::now();
+		duration = duration_cast<milliseconds>(end - start);
+		deleting[2] = duration.count();
+		
+		if (i != 0){
+			results[i][0][0] += results[i-1][0][0];
+                        results[i][0][1] += results[i-1][0][1];
+                        results[i][0][2] += results[i-1][0][2];
+
+                        results[i][1][0] += results[i-1][1][0];
+                        results[i][1][1] += results[i-1][1][1];
+                        results[i][1][2] += results[i-1][1][2];
+
+                        results[i][2][0] += results[i-1][2][0];
+                        results[i][2][1] += results[i-1][2][1];
+                        results[i][2][2] += results[i-1][2][2];
+
+                        results[i][3][0] += results[i-1][3][0];
+                        results[i][3][1] += results[i-1][3][1];
+                        results[i][3][2] += results[i-1][3][2];
+		}
+	}
+	display(reading, sorting, insert, deleting);
+
+	return 0;
 }
 
 void display(int r[], int s[], int i[], int d[]){
-    cout << right << setw(15) << "Operation"
-         << right << setw(10) << "Vector"
-         << setw(10) << "List"
-         << setw(10) << "Set" << endl;
-    cout << right << setw(15) << "Read"
-         << right << setw(10) << r[0]
-         << setw(10) << r[1]
-         << setw(10) << r[2] << endl;
-    cout << right << setw(15) << "Sort"
-         << right << setw(10) << s[0]
-         << setw(10) << s[1]
-         << setw(10) << s[2] << endl;
-    cout << right << setw(15) << "Insert"
-         << right << setw(10) << i[0]
-         << setw(10) << i[1]
-         << setw(10) << i[2] << endl;
-    cout << right << setw(15) << "Delete"
-         << right << setw(10) << d[0]
-         << setw(10) << d[1]
-         << setw(10) << d[2] << endl;
+	cout << right << setw(15) << "Operation"
+		<< right << setw(10) << "Vector"
+		<< setw(10) << "List"
+		<< setw(10) << "Set" << endl;
+	cout << right << setw(15) << "Read"
+		<< right << setw(10) << r[0]
+		<< setw(10) << r[1]
+		<< setw(10) << r[2] << endl;
+	cout << right << setw(15) << "Sort"
+		<< right << setw(10) << s[0]
+		<< setw(10) << s[1]
+		<< setw(10) << s[2] << endl;
+	cout << right << setw(15) << "Insert"
+		<< right << setw(10) << i[0]
+		<< setw(10) << i[1]
+		<< setw(10) << i[2] << endl;
+	cout << right << setw(15) << "Delete"
+		<< right << setw(10) << d[0]
+		<< setw(10) << d[1]
+		<< setw(10) << d[2] << endl;
 }
 /* syntax examples:
-auto start = high_resolution_clock::now()
-auto end = high_resolution_clock::now()
-auto duration = duration_cast<milliseconds>(end - start)
-duration.count() references elapsed milliseconds
-*/
+   auto start = high_resolution_clock::now()
+   auto end = high_resolution_clock::now()
+   auto duration = duration_cast<milliseconds>(end - start)
+   duration.count() references elapsed milliseconds
+ */
